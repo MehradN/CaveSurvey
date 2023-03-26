3c8f0f1d9d73b671d1c6acf9be2872cf1f92b45c
Mehrad Nayyeri <mehrad.nayyeri@zohomail.com>
2023-03-26 20:35:29 +0330

    Initial commit

---
+++ src/main/java/ir/mehradn/cavesurvey/CaveSurvey.java
@@ -0,0 +1,13 @@
+package ir.mehradn.cavesurvey;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CaveSurvey implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("cave-survey");
+
+	public void onInitialize() {
+	}
+}

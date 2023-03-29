3ba4e720779274283e3821570a16f8444421081c
Mehrad Nayyeri <mehrad.nayyeri@zohomail.com>
2023-03-29 09:54:43 +0330

    Added hover text to CaveMapItem

--- src/main/java/ir/mehradn/cavesurvey/item/CaveMapItem.java
+++ src/main/java/ir/mehradn/cavesurvey/item/CaveMapItem.java
@@ -2,6 +2,7 @@ package ir.mehradn.cavesurvey.item;
 
 import eu.pb4.polymer.core.api.item.PolymerItem;
 import ir.mehradn.cavesurvey.mixin.accessor.MapItemAccessor;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -12,7 +13,6 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -35,7 +35,12 @@ public class CaveMapItem extends MapItem implements PolymerItem {
 
     public void update(Level level, Entity viewer, MapItemSavedData data) { } // TODO
 
-    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) { } // TODO
+    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+        super.appendHoverText(stack, level, tooltipComponents, isAdvanced);
+        Integer id = getMapId(stack);
+        if (id != null && !isAdvanced.isAdvanced() && !stack.hasCustomHoverName())
+            tooltipComponents.add(Component.literal("#" + id).withStyle(ChatFormatting.GRAY));
+    }
 
     public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
         return InteractionResultHolder.consume(player.getItemInHand(usedHand));
